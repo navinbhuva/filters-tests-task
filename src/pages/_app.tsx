f66b058b3daa37f8d2725b9ee42f2a6b76079b64
Krunal Shah <shahkrunal7091@gmail.com>
2023-04-10 17:50:13 +0530

    feat: next page layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,28 @@ import type { AppProps } from 'next/app';
 /**
  * * Use fontsource for any custom google fonts. It improves the performance and takes care of preloading.
  * * Check the fontsource documentation if there is a variable version available use it.
- * * https://fontsource.org/docs/introduction
+ * * Learn More: https://fontsource.org/docs/introduction
  */
 import '@fontsource/inter/variable.css';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+/**
+ * * Next Layouts are a better way to apply the layout to the pages.
+ * * Learn More: https://nextjs.org/docs/basic-features/layouts
+ */
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
 };
 
 export default App;
