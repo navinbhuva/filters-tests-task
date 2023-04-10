3b76836282f192a8893d13a6fccd8010107c9bf3
Krunal Shah <shahkrunal7091@gmail.com>
2023-04-10 17:40:45 +0530

    feat: eslint, prettier and fontsource

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+/**
+ * * Use fontsource for any custom google fonts. It improves the performance and takes care of preloading.
+ * * Check the fontsource documentation if there is a variable version available use it.
+ * * https://fontsource.org/docs/introduction
+ */
+import '@fontsource/inter/variable.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
